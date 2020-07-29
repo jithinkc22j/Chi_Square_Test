8898e3877ee636d8ce1d27ec9612f13b453fef46
JITHIN K C <jithinkc22@gmail.com>
2020-07-28 23:58:00 -1000

    Update main.m

--- main.m
+++ main.m
@@ -6,6 +6,10 @@
 
 %---------------------------------------------------------------------------------
 % Demo: 
+
+web('https://docs.google.com/document/d/1AbCxFoUhdOCppM8novgCdOv0F9mqYe7HlBU7yX7Svx0/edit?usp=sharing')
+web('published_work.html')
+
 a=[31 14 45;2 5 53;53 45 2];
 %---------------------------------------------------------------------------
  % Calling chi-test function:-
